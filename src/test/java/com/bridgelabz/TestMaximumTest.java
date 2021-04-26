fe0ba96a9b1398f19fc5fe6f435346f7c0b9cd3f
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-04-26 18:44:12 +0530

    Added : To Check Max Integer Number at Different Place

--- src/test/java/com/bridgelabz/TestMaximumTest.java
+++ src/test/java/com/bridgelabz/TestMaximumTest.java
@@ -13,8 +13,20 @@ public class TestMaximumTest {
     }
 
     @Test
-    public void MaxAtFirstPlace() {
+    public void MaxAtFirstPlaceInteger() {
         int vales = testMaximum.integerMax(55,33,4);
+        Assert.assertEquals(55, vales);
+    }
+
+    @Test
+    public void MaxAtSecondPlaceInteger() {
+        int vales = testMaximum.integerMax(33,55,4);
+        Assert.assertEquals(55, vales);
+    }
+
+    @Test
+    public void MaxAtThirdPlaceInteger() {
+        int vales = testMaximum.integerMax(33,4,55);
         Assert.assertEquals(55, vales);
     }
 }
