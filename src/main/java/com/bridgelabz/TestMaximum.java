70d37fedea6b3a81bfedcfc4612129caaf07b059
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-04-27 07:17:41 +0530

    Refactor : Using One Generic method and find Maximum

--- src/main/java/com/bridgelabz/TestMaximum.java
+++ src/main/java/com/bridgelabz/TestMaximum.java
@@ -1,35 +1,13 @@
 package com.bridgelabz;
 public class TestMaximum{
-
-    public static Integer integerMax(Integer xInt, Integer yInt, Integer zInt) {
-        int max = xInt;
-        if(yInt.compareTo(max) > 0){
-            max = yInt;
+    /* Generic Method to Check Max Integer, Double, String*/
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+        T max = x;
+        if(y.compareTo(max) > 0){
+            max = y;
         }
-        if(zInt.compareTo(max) > 0){
-            max = zInt;
-        }
-        return max;
-    }
-
-    public static Double doubleMax(Double xFlt, Double yFlt, Double zFlt) {
-        double max = xFlt;
-        if(yFlt.compareTo(max) > 0){
-            max = yFlt;
-        }
-        if(zFlt.compareTo(max) > 0){
-            max = zFlt;
-        }
-        return max;
-    }
-    /*Method to Check Max String */
-    public static String stringMax(String xStr, String yStr, String zStr) {
-        String max = xStr;
-        if(yStr.compareTo(max) > 0){
-            max = yStr;
-        }
-        if(zStr.compareTo(max) > 0){
-            max = zStr;
+        if(z.compareTo(max) > 0){
+            max = z;
         }
         return max;
     }
--- src/test/java/com/bridgelabz/TestMaximumTest.java
+++ src/test/java/com/bridgelabz/TestMaximumTest.java
@@ -14,55 +14,55 @@ public class TestMaximumTest {
 
     @Test
     public void MaxAtFirstPlaceInteger() {
-        int vales = testMaximum.integerMax(55,33,4);
+        int vales = testMaximum.maximum(55,33,4);
         Assert.assertEquals(55, vales);
     }
 
     @Test
     public void MaxAtSecondPlaceInteger() {
-        int vales = testMaximum.integerMax(33,55,4);
+        int vales = testMaximum.maximum(33,55,4);
         Assert.assertEquals(55, vales);
     }
 
     @Test
     public void MaxAtThirdPlaceInteger() {
-        int vales = testMaximum.integerMax(33,4,55);
+        int vales = testMaximum.maximum(33,4,55);
         Assert.assertEquals(55, vales);
     }
 
     @Test
     public void MaxAtFirstPlaceDouble() {
-        double vales = testMaximum.doubleMax(55.0,33.2,4.8);
+        double vales = testMaximum.maximum(55.0,33.2,4.8);
         Assert.assertEquals(55.0, vales, 0);
     }
 
     @Test
     public void MaxAtSecondPlaceDouble() {
-        double vales = testMaximum.doubleMax(33.2,55.0,4.8);
+        double vales = testMaximum.maximum(33.2,55.0,4.8);
         Assert.assertEquals(55.0, vales, 0);
     }
 
     @Test
     public void MaxAtThirdPlaceDouble() {
-        double vales = testMaximum.doubleMax(33.2,4.8,55.0);
+        double vales = testMaximum.maximum(33.2,4.8,55.0);
         Assert.assertEquals(55.0, vales, 0);
     }
 
     @Test
     public void MaxAtFirstPlaceString() {
-        String vales = testMaximum.stringMax("Banana","Apple","Peach");
+        String vales = testMaximum.maximum("Banana","Apple","Peach");
         Assert.assertEquals("Peach", vales);
     }
 
     @Test
     public void MaxAtSecondPlaceString() {
-        String vales = testMaximum.stringMax("Apple","Banana","Peach");
+        String vales = testMaximum.maximum("Apple","Banana","Peach");
         Assert.assertEquals("Peach", vales);
     }
 
     @Test
     public void MaxAtThirdPlaceString() {
-        String vales = testMaximum.stringMax("Apple","Peach","Banana");
+        String vales = testMaximum.maximum("Apple","Peach","Banana");
         Assert.assertEquals("Peach", vales);
     }
 }
